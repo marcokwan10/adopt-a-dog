bf26cb729f794c4440863a86daf126a139f5bd09
Marco Kwan <marcokwan10@gmail.com>
2025-02-13 22:36:24 -0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { Analytics } from "@mui/icons-material";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
-// const geistSans = Geist({
-// 	variable: "--font-geist-sans",
-// 	subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-// 	variable: "--font-geist-mono",
-// 	subsets: ["latin"],
-// });
+// import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -30,7 +19,6 @@ export default function RootLayout({
 			<body className="h-full">
 				<AppRouterCacheProvider>
 					{children}
-					<SpeedInsights />
 					<Analytics />
 				</AppRouterCacheProvider>
 			</body>
